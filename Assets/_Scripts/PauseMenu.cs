f2bebe9648f45a2c74556c0b06b2dad9f6facf27
NyamaNyama <tihon31.01.04@mail.ru>
2025-05-03 17:25:01 +1000

    Add PauseMenu

---
+++ Assets/_Scripts/PauseMenu.cs
@@ -0,0 +1,42 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace _Scripts
+{
+    public class PauseMenu : MonoBehaviour
+    {
+        [SerializeField] private GameObject menuWindow;
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                if (menuWindow.activeSelf == true)
+                {
+                    Continue();
+                }
+                else
+                {
+                    Pause();
+                }
+            }
+        }
+
+        public void Pause()
+        {
+            Time.timeScale = 0f;
+            menuWindow.SetActive(true);
+        }
+
+        public void Continue()
+        {
+            Time.timeScale = 1f;
+            menuWindow.SetActive(false);
+        }
+
+        public void MainMenu()
+        {
+            SceneManager.LoadScene("Menu");
+        }
+    }
+}
